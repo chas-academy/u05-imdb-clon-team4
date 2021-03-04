1f1c07e336ce76cbc1722b98b7af878eaa8526aa
Johanna Sjöstrand <johanna.sjoestrand@gmail.com>
2021-03-04 17:30:50 +0100

    add css styles for footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -5,26 +5,34 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
     <script src="https://kit.fontawesome.com/e7d0d2ed9d.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
     <title>Document</title>
 </head>
 <body>
     <footer class="footer .text-white mt-auto py-3 bg-dark fixed-bottom" style="height: 200px;">
-        <div class="footer-container row align-items-start">
-            <h4 class="col" style="color: white; margin-right: 10px;"><a href="#">About</a></h4>
-            <h4 class="col" style="color: white;"><a href="#">FAQ</a></h4>
-            <h4 class="col" style="color: white;"><a href="#">Contact</a></h4>
-            <h4 class="col" style="color: white;"><a href="#">Terms & Conditions</a></h4>
+        <div class="footer-container d-flex justify-content-around">
+            <h4 class="p-2"><a href="#">About</a></h4>
+            <h4 class="p-2" ><a href="#">FAQ</a></h4>
+            <h4 class="p-2"><a href="#">Contact</a></h4>
+            <h4 class="p-2"><a href="#">Terms & Conditions</a></h4>
         </div>
-        <div class="social-container">
-        <a class="btn btn-block btn-social btn-twitter">
-    <span class="fa fa-twitter"></span> Sign in with Twitter
-  </a>
-    <a class="btn btn-social-icon btn-twitter">
-    <span class="fa fa-twitter" style="color: white;"></span>
-  </a>
+
+        <div class="social-container d-flex align-items-end justify-content-center" style="border: 2px solid red;">
+            <button type="button" class="btn btn-social-icon btn-facebook p-2">
+            <i class="fa fa-facebook p-2"></i></button>
+
+            <button type="button" class="btn btn-social-icon btn-youtube p-2">
+            <i class="fa fa-youtube p-2"></i></button>
+
+            <button type="button" class="btn btn-social-icon btn-twitter p-2">
+            <i class="fa fa-twitter p-2"></i></button>
+
+            <button type="button" class="btn btn-social-icon btn-dribbble p-2">
+            <i class="fa fa-dribbble p-2"></i></button>
+
         </div>
+
     </footer>
 </body>
 </html>
