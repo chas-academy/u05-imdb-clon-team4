b5ec55541e1ff1c8333ec7a91256d64fdf624735
Axel Roussille Åberg <axelra82@gmail.com>
2021-03-11 11:24:18 +0100

    fix: change refs to relative root

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,6 @@
   <head>
-      <link rel="stylesheet" href="{{ secure_asset('/css/app.css') }}">
+      {{-- <link rel="stylesheet" href="{{ secure_asset('/css/app.css') }}"> --}}
+      <link rel="stylesheet" href="/css/app.css">
       <script src="https://kit.fontawesome.com/e7d0d2ed9d.js" crossorigin="anonymous"></script>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous" />
@@ -8,7 +9,8 @@
       </script>
       <script src="https://code.jquery.com/jquery-3.5.1.min.js"
           integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-      <script src="{{ secure_asset('/resources/js/app.js')}}" defer></script>
+      {{-- <script src="{{ secure_asset('/resources/js/app.js')}}" defer></script> --}}
+      <script src="/resources/js/app.js" defer></script>
       <meta charset="UTF-8" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
