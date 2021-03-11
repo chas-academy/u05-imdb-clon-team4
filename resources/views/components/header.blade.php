240f6852170905b54ed9e615de4c5b5a5cff5d6f
Johanna Sjöstrand <johanna.sjoestrand@gmail.com>
2021-03-11 11:11:00 +0100

    add secure asset

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,5 @@
   <head>
-      <link rel="stylesheet" href="https://imdb-clone-footer-2d7nlp0byhc3.herokuapp.com/css/app.css">
+      <link rel="stylesheet" href="{{ secure_asset('/css/app.css') }}">
       <script src="https://kit.fontawesome.com/e7d0d2ed9d.js" crossorigin="anonymous"></script>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous" />
