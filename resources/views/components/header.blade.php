ff72fc68f23d1e36e03cfb44a996ffaa789dff3b
Johanna Sjöstrand <johanna.sjoestrand@gmail.com>
2021-03-11 10:32:16 +0100

    try with css

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,5 @@
   <head>
-      <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+      <link rel="stylesheet" href="/resources/css/app.css">
       <script src="https://kit.fontawesome.com/e7d0d2ed9d.js" crossorigin="anonymous"></script>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous" />
@@ -8,7 +8,7 @@
       </script>
       <script src="https://code.jquery.com/jquery-3.5.1.min.js"
           integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-      <link rel="stylesheet" href="style.css" />
+      {{--  <link rel="stylesheet" href="style.css" /> --}}
       <script src="script.js" defer></script>
       <meta charset="UTF-8" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
