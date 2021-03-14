a56aff0bb1323844f8a1367a2c8c127643959569
Axel Roussille Åberg <axelra82@gmail.com>
2021-03-14 16:02:13 +0100

    fix: structure footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,9 @@
-<footer class="footer mt-auto py-3 bg-dark fixed-bottom">
-  <div class="footer-container">
-    <span class="text-muted">Text here</span>
+<footer class="footer mt-auto bg-dark fixed-bottom p-3">
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <p class="text-muted">Text here</p>
+      </div>
+    </div>
   </div>
 </footer>
