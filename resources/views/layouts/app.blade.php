a95fbe4f39ff7c025a4d7bcf7664acb5e98d5a6e
Axel Roussille Åberg <axelra82@gmail.com>
2021-03-15 14:09:03 +0100

    fix: add font awesome to header and seperate header layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,16 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>IMDB Clone</title>
-    {{-- Bootstrap --}}
-    {{-- Scripts --}}
-    <script src="{{ secure_asset('js/app.js') }}" defer></script>
-    {{-- Styles --}}
-    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
-  </head>
+  {{-- Include our header --}}
+  @include('layouts.header')
   
   <body>
     {{-- Include our main navigation --}}
