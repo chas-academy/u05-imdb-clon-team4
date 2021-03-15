2f7e2bf351e68e811f2e75370fd119718b2aa30c
Axel Roussille Åberg <axelra82@gmail.com>
2021-03-15 13:49:07 +0100

    fix: move footer branch content to current

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,9 +1,21 @@
-<footer class="footer mt-auto bg-dark fixed-bottom p-3">
-  <div class="container">
-    <div class="row">
-      <div class="col">
-        <p class="text-muted">Text here</p>
-      </div>
-    </div>
+<footer class="footer .text-white mt-auto py-3 bg-dark fixed-bottom" style="height: 200px;">
+  <div class="footer-container d-flex justify-content-around">
+    <h4 class="p-2"><a href="#">About</a></h4>
+    <h4 class="p-2"><a href="#">FAQ</a></h4>
+    <h4 class="p-2"><a href="#">Contact</a></h4>
+    <h4 class="p-2"><a href="#">Terms & Conditions</a></h4>
+  </div>
+  <div class="social-container d-flex align-items-end justify-content-center" style="border: 2px solid red;">
+    <button type="button" class="btn btn-social-icon btn-facebook p-2">
+        <i class="fa fa-facebook p-2"></i></button>
+
+    <button type="button" class="btn btn-social-icon btn-youtube p-2">
+        <i class="fa fa-youtube p-2"></i></button>
+
+    <button type="button" class="btn btn-social-icon btn-twitter p-2">
+        <i class="fa fa-twitter p-2"></i></button>
+
+    <button type="button" class="btn btn-social-icon btn-dribbble p-2">
+        <i class="fa fa-dribbble p-2"></i></button>
   </div>
 </footer>
