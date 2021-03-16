99e112c977600961e8a2ce9e5d14054cf352e639
Karin Stenwall <karin.stenwall@gmail.com>
2021-03-16 21:50:21 +0100

    reset accidental commits to wrong branch...

--- resources/views/components/list.blade.php
+++ resources/views/components/list.blade.php
@@ -1,42 +1,24 @@
-<div class="card-container d-flex flex-column p-2">
 
-    <div id="top-rated-container" class="container mb-5">
-		<h2 class="">Top rated titles</h2>
-		<div class="row gx-2">
-    {{-- fortsätta här om vi vill ha lådor med klickbara listor --}}
+    <div class="d-flex flex-column align-items-center card-container">
+           <h2 class="text-center mt-3">Other content below</h2>
 
-    		@for($i = 0; $i <= 4; $i++)
-			<div class="col">
-    			<div class="card p-0 border-0 rounded-0">
-					<img class="card-img-top rounded-0" src="https://image.posterlounge.se/images/l/1875927.jpg" alt="Card image cap">
-        			<div class="card-body rounded-0">
-            			<h2 class="card-title h5">Movie title</h2>
-						<h3 class="card-subtitle mb-2 text-muted h6">Card subtitle</h3
-            			<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        			</div>
-   				</div>
-			</div>
-    		@endfor
+        <div class="container row p-2 gx-5">
+        {{-- fortsätta här om vi vill ha lådor med klickbara listor --}}
 
-		</div>
-	</div>
 
-	<div id="news-container" class="container mb-5">
-		<div class="row gx-2">
+          @for($i = 0; $i <= 5; $i++)
+        <div class="card text-dark bg-light col m-3" style="max-width: 18rem;">
 
-    		@for($i = 0; $i <= 4; $i++)
-			<div class="col">
-    			<div class="card p-0 border-0 rounded-0">
-					<img class="card-img-top rounded-0" src="https://image.posterlounge.se/images/l/1875927.jpg" alt="Card image cap">
-        			<div class="card-body rounded-0">
-            			<h2 class="card-title h5">Movie title</h2>
-            			<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        			</div>
-   				</div>
-			</div>
-    		@endfor
+            <div class="card-header">Header</div>
+            <div class="card-body">
+                <h5 class="card-title">Light card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            </div>
+            @endfor
 
-		</div>
-	</div>
 
-</div>
+            </div>
+            </div>
+
+    </div>
