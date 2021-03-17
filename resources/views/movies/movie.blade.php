89a173f9c9f572bdc1e810e7e99f6f9f37be3868
Johanna Sjöstrand <johanna.sjoestrand@gmail.com>
2021-03-17 15:06:06 +0100

    start of single  movie view

--- resources/views/movies/movie.blade.php
+++ resources/views/movies/movie.blade.php
@@ -1,12 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Movie</title>
-</head>
-<body>
- <h1> Movie's </h1>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+
+ <h1> Movie Title </h1>
+ <button>Add movie to list</button>
+
+ <img src="/public/images/hollywood.jpg">
+
+ <h2>Summary</h2>
+
+ <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quidem iste laudantium asperiores, dolorem illo quibusdam voluptatem earum maiores nulla quisquam ratione et iusto deleniti eos perspiciatis sed dignissimos. Esse.
+ Amet aliquid soluta autem. Ipsum aperiam ipsam accusantium magnam? Minima ipsa eveniet dolores illo porro, veritatis asperiores dicta ratione vel fugit consequuntur temporibus reiciendis. Ullam laboriosam sequi ipsa fuga aut.
+ Doloremque autem optio exercitationem harum dicta ipsa magni! Impedit molestias distinctio, accusamus tempora quis qui esse fugit deleniti minus. Earum natus sapiente quisquam maxime ratione hic harum, quo ducimus eum!
+ Maiores et distinctio vero sequi neque eligendi doloribus recusandae sit placeat ipsum maxime, dignissimos nulla esse commodi harum mollitia odit blanditiis! Aut architecto corrupti recusandae molestias quisquam id reiciendis ut.
+ Deleniti, vitae asperiores! Soluta quae voluptatum sapiente, voluptatem natus sint nisi maiores expedita possimus voluptas cupiditate animi facere deleniti saepe eum illo error officiis praesentium odit. Eveniet sint quis esse!
+ Voluptas dolorem maxime amet omnis expedita eos architecto ipsum nisi mollitia labore, ullam, unde laborum consequatur laudantium voluptatem rem animi quae quibusdam explicabo! Hic deleniti quod non sit explicabo ducimus.
+ Incidunt officia assumenda exercitationem provident et sunt a sed soluta cupiditate, facere deleniti facilis aliquid est enim ab aspernatur debitis odio eos! Sunt quisquam officia ex earum saepe, deserunt obcaecati!
+ Mollitia iusto ducimus consequatur ea sed dicta nostrum id explicabo tempore sunt assumenda libero voluptatum vitae autem dolorum laborum molestias illum iste accusantium perferendis, quo quibusdam odio. Placeat, in neque.
+ Vitae laborum iste, officia a quae eos aperiam tenetur dolorum magni aut sed voluptatibus non itaque nam veniam ex quibusdam pariatur, distinctio quaerat laboriosam ullam ut. Possimus rem dolores fugit?
+</p>
+
+
+@endsection
