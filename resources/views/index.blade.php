5dcf2df2ede999aa8ebd26f86cea27cf9f931a6e
Karin Stenwall <karin.stenwall@gmail.com>
2021-03-18 15:37:54 +0100

    change structure for templating index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Main index page</h1>
-    <p>Lists and stuff goes here...</p>
+    <h1>Welcome to the IMDB Clone by team 4!</h1>
+
+    {{-- Include carousel --}}
+	@include('components.carousel')
+
+    {{-- Include list --}}
+    @include('components.list')
 @endsection
