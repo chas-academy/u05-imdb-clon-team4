85d720b496842067286572f51e17ae8cb0303f73
Braggedtooth <54339202+Braggedtooth@users.noreply.github.com>
2021-03-18 14:58:02 +0100

    Update footer.blade.php
    Fixed footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,21 +1,21 @@
-<footer class="footer .text-white mt-auto py-3 bg-dark" style="height: 200px;">
+<footer class="footer text-white mt-auto py-3 bg-dark fixed-bottom">
   <div class="footer-container d-flex justify-content-around">
     <h4 class="p-2"><a href="#">About</a></h4>
     <h4 class="p-2"><a href="#">FAQ</a></h4>
     <h4 class="p-2"><a href="#">Contact</a></h4>
     <h4 class="p-2"><a href="#">Terms & Conditions</a></h4>
   </div>
-  <div class="d-flex justify-content-center mt-5">
-    <button type="button" class="btn btn-social-icon btn-facebook p-2">
-        <i class="fa fa-facebook p-4"></i></button>
+  <div class="d-flex justify-content-center my-4">
+    <button type="button" class="btn btn-social-icon btn-facebook p-3">
+        <i class="fa fa-facebook p-1"></i></button>
 
-    <button type="button" class="btn btn-social-icon btn-youtube p-2">
-        <i class="fa fa-youtube p-4"></i></button>
+    <button type="button" class="btn btn-social-icon btn-youtube p-3">
+        <i class="fa fa-youtube p-1"></i></button>
 
-    <button type="button" class="btn btn-social-icon btn-twitter p-2">
-        <i class="fa fa-twitter p-4"></i></button>
+    <button type="button" class="btn btn-social-icon btn-twitter p-3">
+        <i class="fa fa-twitter p-1"></i></button>
 
-    <button type="button" class="btn btn-social-icon btn-dribbble p-2">
-        <i class="fa fa-dribbble p-4"></i></button>
+    <button type="button" class="btn btn-social-icon btn-dribbble p-3">
+        <i class="fa fa-dribbble p-1"></i></button>
   </div>
 </footer>
