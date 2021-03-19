7d65f8f06a0b9c022a177eb1f8f586db842211c7
Karin Stenwall <karin.stenwall@gmail.com>
2021-03-19 21:06:56 +0100

    fix indentation

--- resources/views/components/cards.blade.php
+++ resources/views/components/cards.blade.php
@@ -1,10 +1,10 @@
 <div class="card-container d-flex flex-column p-2">
 
     <div id="top-rated-container" class="container mb-4">
-		<div class="mt-3 mb-4">
-        	<h2 class="">Top new picks</h2>
-			<h3 class="h5">Some sub-title</h3>
-		</div>
+        <div class="mt-3 mb-4">
+            <h2 class="">Top new picks</h2>
+            <h3 class="h5">Some sub-title</h3>
+        </div>
         <div class="row gx-4 flex-nowrap">
     {{-- fortsätta här om vi vill ha lådor med klickbara listor --}}
 
@@ -14,10 +14,10 @@
                     <img class="card-img-top rounded-0" src="https://image.posterlounge.se/images/l/1875927.jpg" alt="Card image cap">
                     <div class="card-body rounded-0">
                         <h3 class="card-title h5">Movie title</h2>
-						<p class="card-subtitle mb-2 text-muted h6">Year <span></span></p>
+                        <p class="card-subtitle mb-2 text-muted h6">Year <span></span></p>
                     </div>
                 </div>
-            </div>
+               </div>
             @endfor
 
         </div>
