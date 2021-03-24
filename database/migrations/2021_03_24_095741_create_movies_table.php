8c6f6a897c9e9f8063e77f504b1c6a0e09da4c95
Axel Roussille Åberg <axelra82@gmail.com>
2021-03-24 11:22:54 +0100

    fix: change image to binary and alter to longblob for base64

---
+++ database/migrations/2021_03_24_095741_create_movies_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->date('year');
+            $table->binary('image')->nullable();
+            $table->timestamps();
+        });
+
+        // Blueprint can only set BLOB type (binary)
+        // We need to set it to LONGBLOB for base64 image storage
+        DB::statement("ALTER TABLE `movies` MODIFY `image` LONGBLOB");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
