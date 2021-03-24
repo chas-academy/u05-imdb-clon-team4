c5dcad5aa0d55beff844b8e5dc4c188d92abc39f
Johanna Sjöstrand <johanna.sjoestrand@gmail.com>
2021-03-24 21:38:03 +0100

    add terms route and basic view

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -3,7 +3,7 @@
     <h4 class="p-2"><a href="#">About</a></h4>
     <h4 class="p-2"><a href="#">FAQ</a></h4>
     <h4 class="p-2"><a href="#">Contact</a></h4>
-    <h4 class="p-2"><a href="#">Terms & Conditions</a></h4>
+    <h4 class="p-2"><a href="{{route('user_terms')}}">Terms & Conditions</a></h4>
   </div>
   <div class="d-flex justify-content-center my-4">
     <button type="button" class="btn btn-social-icon btn-facebook p-3">
