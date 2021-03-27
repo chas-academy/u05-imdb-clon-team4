303a9ef6630258778bea2dab739eb5036010b64a
Axel Roussille Åberg <axelra82@gmail.com>
2021-03-27 12:26:55 +0100

    chore: new status_id

--- app/Http/Requests/StatusRequest.php
+++ app/Http/Requests/StatusRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use App\Http\Requests\Request;
 use Illuminate\Foundation\Http\FormRequest;
 
-class MovieRequest extends FormRequest
+class StatusRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -26,10 +26,7 @@ class MovieRequest extends FormRequest
     public function rules()
     {
         return [
-            'status_id' => 'required',
-            'title' => 'required|min:5',
-            'year' => 'required',
-            'description' => 'required|min:5',
+            // 'name' => 'required|min:5|max:255'
         ];
     }
 
