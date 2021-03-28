121bacdf6a860afd4973fe013ec37e482be6329e
Karin Stenwall <karin.stenwall@gmail.com>
2021-03-28 18:04:10 +0200

    line up reviews

--- resources/views/components/review-card.blade.php
+++ resources/views/components/review-card.blade.php
@@ -3,7 +3,7 @@
     $userIsReviewer = !auth()->user() || auth()->user()->name !== $review->user_name;
 @endphp
 <div class="col">
-	<div class="card p-0 border-0 rounded-0">
+	<div class="card p-0 border-0 rounded-0 h-100">
     	<h3 class="card-header bg-primary rounded-0 h5">{{ $review->title }}</h3>
     	<div class="card-body rounded-0">
         	<p class="card-text">{!! $review->description !!}</p>
