08f149ce693a0aad34157eae94a460be15bfdeb3
Karin Stenwall <karin.stenwall@gmail.com>
2021-03-29 15:51:52 +0200

    move functionality from cards to page

--- resources/views/includes/cards.blade.php
+++ resources/views/includes/cards.blade.php
@@ -1,9 +1,7 @@
-<div class="col movie-cards-col">
-    <a href="movie/{{ $movies[$i]->id }}" class="card p-0 border-0 rounded-0 h-100">
-        <img class="card-img-top rounded-0" src="{{ $movies[$i]->image }}" alt="{{ $movies[$i]->title }}">
-        <div class="card-body rounded-0">
-            <h3 class="card-title h5">{{ $movies[$i]->title }}</h2>
-            <p class="card-subtitle mb-2 text-muted h6">{{ substr($movies[$i]->year, 0, 4) }}</p>
-        </div>
-    </a>
-</div>
+<a href="movie/{{ $movies[$i]->id }}" class="card p-0 border-0 rounded-0 h-100">
+    <img class="card-img-top rounded-0" src="{{ $movies[$i]->image }}" alt="{{ $movies[$i]->title }}">
+    <div class="card-body rounded-0">
+        <h3 class="card-title h5">{{ $movies[$i]->title }}</h2>
+        <p class="card-subtitle mb-2 text-muted h6">{{ substr($movies[$i]->year, 0, 4) }}</p>
+    </div>
+</a>
