91fd3e43faf54b430ac07932c6813cde2388c688
Malte <malte.hallstrom@gmail.com>
2021-03-29 21:01:41 +0200

    feat: fix carousel logic and add images
    The carousel now works and cover art is fetched from TMDB.

--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -1,88 +1,44 @@
-<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-      <div class="carousel-indicators">
-        <button
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide-to="0"
-          class="active"
-          aria-current="true"
-          aria-label="Slide 1"
-        ></button>
-        <button
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide-to="1"
-          aria-label="Slide 2"
-        ></button>
-        <button
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide-to="2"
-          aria-label="Slide 3"
-        ></button>
-      </div>
-      <div class="container">
-        <div class="carousel-inner p-3">
-          <div class="carousel-item active">
-            <img
-              src="images/yesterday.jpg"
-              class="d-inline-block w-100"
-              alt="..."
-              height="600"
-            />
-            <div class="carousel-caption d-none d-md-block">
-              <h5>First slide label</h5>
-              <p>
-                Some representative placeholder content for the first slide.
-              </p>
+<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+    </ol>
+
+    <div class="carousel-inner" role="listbox">
+        @foreach ($movies as $movie)
+            <div class="item{{ $loop->index === 0 ? " active" : "" }}" onclick="window.location.href='/movie/{{ $movie->id }}'">
+                <img
+                src="https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces{{ $movie->backdrop }}"
+                class="d-inline-block w-100"
+                alt="{{$movie->title}}"
+                height="500"
+                />
+                <div class="carousel-caption d-none d-md-block">
+                <h2>{{ $movie->title }}</h2>
+                <h3>{{ $movie->tagline }}</h3>
+                <p>{{ $movie->description }}</p>
+                </div>
             </div>
-          </div>
-          <div class="carousel-item">
-            <img
-              src="images/hollywood.jpg"
-              class="d-inline-block w-100"
-              alt="..."
-              height="600"
-            />
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Second slide label</h5>
-              <p>
-                Some representative placeholder content for the second slide.
-              </p>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img
-              src="images/midsommar.jpg"
-              class="d-inline-block w-100"
-              alt="..."
-              height="600"
-            />
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Third slide label</h5>
-              <p>
-                Some representative placeholder content for the third slide.
-              </p>
-            </div>
-          </div>
-        </div>
-        <button
-          class="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide="prev"
-        >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button
-          class="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide="next"
-        >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-      </div>
+        @endforeach
     </div>
+
+    <a
+    class="left carousel-control"
+    role="button"
+    href="#carouselExampleCaptions"
+    data-slide="prev"
+    >
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a
+        class="right carousel-control"
+        role="button"
+        href="#carouselExampleCaptions"
+        data-slide="next"
+    >
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
