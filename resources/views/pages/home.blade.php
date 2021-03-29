4f2d57980b58c039457b69a8774fb7e9ff673554
Karin Stenwall <karin.stenwall@gmail.com>
2021-03-29 14:54:11 +0200

    move loops from include to page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -8,10 +8,14 @@
 
         {{-- Do we have movies to show --}}
         @if (count($movies) > 0)
-            {{-- Include cards --}}
+
             <h2 class="mt-3 mb-4">Some movies to check out</h2>
-	        <div class="row gx-4 flex-nowrap">
-                @include('includes.cards', ['movies' => $movies])
+	        <div class="row g-4 flex-column flex-wrap flex-md-row flex-md-nowrap justify-content-evenly align-content-around">
+				{{-- Loop movies, stop on COUNT_MOVIES or max 5 --}}
+				@for($i = 0; $i < count($movies) && $i < 5; $i++)
+					{{-- Include cards --}}
+                	@include('includes.cards', ['movies' => $movies])
+				@endfor
             </div>
         @endif
 
