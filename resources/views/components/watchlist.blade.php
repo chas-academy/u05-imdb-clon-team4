22195487c3542ebc60ebc571a38c9add2f249877
Karin Stenwall <karin.stenwall@gmail.com>
2021-03-29 15:51:34 +0200

    include cards in watchlist

--- resources/views/components/watchlist.blade.php
+++ resources/views/components/watchlist.blade.php
@@ -1,36 +1,13 @@
-<h1>Watchlist</h1>
-
-
-@foreach ($movies as $movie)
-
-
-
-<h3 class=" title ">{{ $movie->title }}</h3>
-<div class="d-flex col">
-    <p class="p-2">{{ $movie->year }}</p>
-</div>
-<div class="col-sm-12 col-md-12 col-lg-6">
-     <form action="{{route('remove_from_watchlist', $movie->id)}}" method="post">
-        @csrf
-        @method('DELETE')
-        <button type="submit" class="btn btn-warning m-2 p-2 col-sm-6 col-lg-2">Delete</button>
-    </form>
-</div>
-
-
-<section class="col-sm-12 col-md-12 d-lg-flex justify-content-lg-start flex-row  h-auto ">
-
-    <div class="row my-auto col-lg-12">
-        <div class="col-lg-5 h-50">
-            <img src="{{ $movie->image }}" alt="{{ $movie->title }}" />
-        </div>
-
-        <div class="col-sm-12 col-lg-5  h-50">
-            <h2 class="mt-2">Storyline</h2>
-            <p class="my-5">{{ $movie->description }}</p>
-        </div>
-    </div>
-
-</section>
-
-@endforeach
+@for($i = 0; $i < count($movies); $i++)
+	<div class="col movie-cards-col h-100">
+		{{-- Include cards --}}
+    	@include('includes.cards', ['movies' => $movies])
+		<div class="card-footer mb-4">
+     		<form action="{{route('remove_from_watchlist', $movies[$i]->id)}}" method="post" >
+        		@csrf
+        		@method('DELETE')
+        		<button type="submit" class="btn btn-sm btn-warning my-2 col-12">Remove</button>
+    		</form>
+		</div>
+	</div>
+@endfor
