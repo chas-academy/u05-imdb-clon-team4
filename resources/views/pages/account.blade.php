d411eb2dac10afceb6a8cbdb4557aef38468065e
Karin Stenwall <karin.stenwall@gmail.com>
2021-03-29 15:56:10 +0200

    new copy if user has no movies in watchlist

--- resources/views/pages/account.blade.php
+++ resources/views/pages/account.blade.php
@@ -5,11 +5,13 @@
 
 	{{-- Do we have movies to show --}}
     @if (count($movies) > 0)
-		<h2 class="h4 mt-3 mb-4">Here is your watchlist</h2>
+		<h2 class="h3 mt-3 mb-4">Here is your watchlist</h2>
 		<div class="row g-4 flex-row">
 
 			@include('components.watchlist')
 		</div>
+	@else
+		<h2 class="h3 mt-3 mb-4">If you add some movies to your watchlist, they will end up here</h2>
 
 	@endif
 
