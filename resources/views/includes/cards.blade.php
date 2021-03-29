f3b7f160f789c507120f45c855c9b0fd2b1e7f4e
Karin Stenwall <karin.stenwall@gmail.com>
2021-03-29 09:33:47 +0200

    fix indentation and formatting

--- resources/views/includes/cards.blade.php
+++ resources/views/includes/cards.blade.php
@@ -1,12 +1,12 @@
 {{-- Loop movies, stop on COUNT_MOVIES or max 5 --}}
-	@for($i = 0; $i < count($movies) && $i < 5; $i++)
-		<div class="col">
-			<a href="movie/{{ $movies[$i]->id }}" class="card p-0 border-0 rounded-0">
-				<img class="card-img-top rounded-0" src="{{ $movies[$i]->image }}" alt="{{ $movies[$i]->title }}">
-				<div class="card-body rounded-0">
-					<h3 class="card-title h5">{{ $movies[$i]->title }}</h2>
-					<p class="card-subtitle mb-2 text-muted h6">{{ $movies[$i]->year }}</p>
-				</div>
-			</a>
-		</div>
-	@endfor
+@for($i = 0; $i < count($movies) && $i < 5; $i++)
+    <div class="col">
+        <a href="movie/{{ $movies[$i]->id }}" class="card p-0 border-0 rounded-0">
+            <img class="card-img-top rounded-0" src="{{ $movies[$i]->image }}" alt="{{ $movies[$i]->title }}">
+            <div class="card-body rounded-0">
+                <h3 class="card-title h5">{{ $movies[$i]->title }}</h2>
+                <p class="card-subtitle mb-2 text-muted h6">{{ $movies[$i]->year }}</p>
+            </div>
+        </a>
+    </div>
+@endfor
