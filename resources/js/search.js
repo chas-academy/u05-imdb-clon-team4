c935414eda37c5dafc924d37db203788fd9f32a8
Karin Stenwall <karin.stenwall@gmail.com>
2021-03-30 14:39:48 +0200

    fix year in search results

--- resources/js/search.js
+++ resources/js/search.js
@@ -43,18 +43,18 @@ const searchFunction = async () => {
             for (let i = 0; i < response.length; i++) {
                 const movie = response[i];
                 results += `<a class="card my-2 mx-2 p-0" style="max-width: 30rem;" href="/movie/${movie.id}">
-					<div class="row g-0">
-					<div class="col-3">
-					<img src="${movie.image}" alt="${movie.title}" style="height: 7rem;"/>
-					</div>
-					<div class="col-9">
-					<div class="card-body">
-					<h2 class="card-title h5">${movie.title}</h2>
-					<h3 class="card-subtitle h6">${movie.year}</h3>
-					</div>
-					</div>
-					</div>
-					</a>`;
+                    <div class="row g-0">
+                    <div class="col-3">
+                    <img src="${movie.image}" alt="${movie.title}" style="height: 7rem;"/>
+                    </div>
+                    <div class="col-9">
+                    <div class="card-body">
+                    <h2 class="card-title h5">${movie.title}</h2>
+                    <h3 class="card-subtitle h6">${movie.year.substring(0, 4)}</h3>
+                    </div>
+                    </div>
+                    </div>
+                    </a>`;
             }
 
             searchResults.innerHTML = results;
