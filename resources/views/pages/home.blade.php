7c10463dc55982b557f77d01019c2e56536e30da
Karin Stenwall <karin.stenwall@gmail.com>
2021-03-31 12:09:23 +0200

    match home-page to main branch

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,18 +2,18 @@
 @extends('layouts.default')
 
 @section('content')
-    <h1>Welcome to the IMDB Clone by team 4!</h1>
+    <h1 class="mb-5">Welcome to the IMDB Clone by team 4!</h1>
 
     {{-- Include carousel --}}
     @include('components.carousel', ["movies" => $latestMovies])
 
     {{-- Do we have movies to show --}}
     @if (count($movies) > 0)
-        <h2 class="mt-3 mb-4 h3">Some movies to check out</h2>
-        <div class="row g-4 flex-column flex-wrap flex-md-row flex-md-nowrap justify-content-evenly align-content-around">
+        <h2 class="mt-5 mb-5 h3">Some movies to check out</h2>
+        <div class="row g-4 flex-column flex-wrap flex-lg-row flex-lg-nowrap justify-content-evenly align-content-around">
             {{-- Loop movies, stop on COUNT_MOVIES or max 5 --}}
             @for($i = 0; $i < count($movies) && $i < 5; $i++)
-                <div class="col movie-cards-col">
+                <div class="col movie-cards-col home-cards">
                     {{-- Include cards --}}
                     @include('components.cards', ['movies' => $movies])
                 </div>
