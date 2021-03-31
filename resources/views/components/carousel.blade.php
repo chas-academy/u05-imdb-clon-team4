c303f82a1eb697d08749e6a32868a2b795316427
Karin Stenwall <karin.stenwall@gmail.com>
2021-03-31 10:14:20 +0200

    change caption font-sizes

--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -1,8 +1,8 @@
 <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Feature movie 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Feature movie 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Feature movie 3"></button>
   </div>
 
     <div class="carousel-inner">
@@ -15,8 +15,8 @@
                 height="500"
                 />
                 <div class="carousel-caption d-none d-md-block">
-                <h2 class="">{{ $movie->title }}</h2>
-                <h3 class="">{{ $movie->tagline }}</h3>
+                <h3>{{ $movie->title }}</h3>
+                <h5>{{ $movie->tagline }}</h5>
                 <p class="fs-6">{{ $movie->description }}</p>
                 </div>
             </div>
