c8a431c8ae789390bf1acc8d2a30a606cfe64286
Karin Stenwall <karin.stenwall@gmail.com>
2021-03-31 10:35:18 +0200

    fix indentation

--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -1,9 +1,9 @@
 <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-  	<div class="carousel-indicators">
-    	<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Feature movie 1"></button>
-    	<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Feature movie 2"></button>
-    	<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Feature movie 3"></button>
-  	</div>
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Feature movie 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Feature movie 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Feature movie 3"></button>
+      </div>
 
     <div class="carousel-inner">
         @foreach ($movies as $movie)
@@ -15,7 +15,7 @@
                 height="500"
             />
             <div class="carousel-caption d-none d-md-block">
-            <a href="movie/{{ $movie->id }}" class="h3">{{ $movie->title }}</a>
+            <h3>{{ $movie->title }}</h3>
             <h5>{{ $movie->tagline }}</h5>
             <p class="fs-6">{{ $movie->description }}</p>
             </div>
@@ -23,13 +23,13 @@
         @endforeach
     </div>
 
-	<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    	<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    	<span class="visually-hidden">Previous</span>
-  	</button>
-  	<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    	<span class="carousel-control-next-icon" aria-hidden="true"></span>
-    	<span class="visually-hidden">Next</span>
-  	</button>
-    
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+
 </div>
